47f019198a83be9669a4db2e8a2e217bd193bb0b
Benjamin Bariteau <bariteau@yelp.com>
2015-07-30 11:25:28 -0700

    Beginning of a Model type. Test and test sqlite db to test it with.

---
+++ model.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Model struct {
+	db *sql.DB
+}
+
+/*
+NewModel creates a new model with a DB connection to the give dbPath (currently sqlite)
+*/
+func NewModel(dbPath string) (m Model, err error) {
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return
+	}
+	m.db = db
+	return
+}
+
+func (m Model) Close() error {
+	return m.db.Close()
+}
---
+++ model_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewModel(t *testing.T) {
+	model, err := NewModel("./test.db")
+	if err != nil {
+		t.Error("Got non-nil error: ", err)
+	}
+	if model.db == nil {
+		t.Error("Got nil db connection")
+	}
+}
