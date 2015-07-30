363ac7b66fa57ab07b2ce3117b6140e1c33d0182
Benjamin Bariteau <bariteau@yelp.com>
2015-07-30 13:48:06 -0700

    Adding GetQuote model method.

--- model/model.go
+++ model/model.go
@@ -2,12 +2,31 @@ package model
 
 import (
 	"database/sql"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type Model struct {
 	db *sql.DB
+}
+
+type Quote struct {
+	ID          int
+	Text        string
+	Score       int
+	TimeCreated time.Time
+	IsOffensive bool
+	IsNishbot   bool
+}
+
+type rawQuote struct {
+	ID          int
+	Text        string
+	Score       int
+	TimeCreated int
+	IsOffensive int
+	IsNishbot   int
 }
 
 /*
@@ -20,6 +39,34 @@ func NewModel(dbPath string) (m Model, err error) {
 	}
 	m.db = db
 	return
+}
+
+func (m Model) GetQuote(id int) (quote Quote, err error) {
+	rawQ := rawQuote{}
+
+	err = m.db.QueryRow(
+		"SELECT id, text, score, time_created, is_offensive, is_nishbot from quote where id = ?",
+		id,
+	).Scan(
+		&rawQ.ID,
+		&rawQ.Text,
+		&rawQ.Score,
+		&rawQ.TimeCreated,
+		&rawQ.IsOffensive,
+		&rawQ.IsNishbot,
+	)
+	if err != nil {
+		return
+	}
+
+	return Quote{
+		ID:          rawQ.ID,
+		Text:        rawQ.Text,
+		Score:       rawQ.Score,
+		TimeCreated: time.Unix(int64(rawQ.TimeCreated), 0),
+		IsOffensive: rawQ.IsOffensive != 0,
+		IsNishbot:   rawQ.IsNishbot != 0,
+	}, nil
 }
 
 func (m Model) Close() error {
--- model/model_test.go
+++ model/model_test.go
@@ -1,7 +1,12 @@
 package model
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestNewModel(t *testing.T) {
@@ -11,5 +16,90 @@ func TestNewModel(t *testing.T) {
 	}
 	if model.db == nil {
 		t.Error("Got nil db connection")
+	}
+}
+
+type TestModel struct {
+	m      Model
+	dbfile *os.File
+}
+
+func (tm TestModel) Close() {
+	tm.m.Close()
+	if tm.dbfile != nil {
+		dbfilepath, err := filepath.Abs(filepath.Dir(tm.dbfile.Name()))
+		if err != nil {
+			os.Remove(dbfilepath)
+		}
+	}
+}
+
+func createTestModel() (tm TestModel, err error) {
+	// create temp sqlite DB
+	f, err := ioutil.TempFile("", "quotedb")
+	if err != nil {
+		return
+	}
+	tm.dbfile = f
+
+	// read schema file
+	schemaBytes, err := ioutil.ReadFile("../schema/quote.sql")
+	if err != nil {
+		return
+	}
+	schema := string(schemaBytes)
+
+	// open db conn
+	m, err := NewModel(f.Name())
+	if err != nil {
+		return
+	}
+	tm.m = m
+
+	// create table(s)
+	_, err = m.db.Exec(schema)
+	if err != nil {
+		return
+	}
+
+	_, err = m.db.Exec(
+		"insert into quote(id, text, score, time_created, is_offensive, is_nishbot) values(?, ?, ?, ?, ?, ?)",
+		1,
+		"fart joke",
+		0,
+		0,
+		0,
+		0,
+	)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func TestGetQuote(t *testing.T) {
+	tm, err := createTestModel()
+	defer tm.Close()
+	if err != nil {
+		t.Error("Got unexpected error: ", err)
+	}
+
+	quote, err := tm.m.GetQuote(1)
+	if err != nil {
+		t.Error("Got non-nil error: ", err)
+	}
+
+	expected := Quote{
+		ID:          1,
+		Text:        "fart joke",
+		Score:       0,
+		TimeCreated: time.Unix(0, 0),
+		IsOffensive: false,
+		IsNishbot:   false,
+	}
+
+	if !reflect.DeepEqual(quote, expected) {
+		t.Error("Got: ", quote, "\nExpected:", expected)
 	}
 }
