2a2f89498dd37033fac2a984e053c8e1bf4ae3ad
Benjamin Bariteau <bariteau@yelp.com>
2015-07-30 20:10:42 -0700

    Adding submission post endpoint (adding quotes works!)

--- irq.go
+++ irq.go
@@ -3,15 +3,15 @@ package main
 import (
 	"github.com/firba1/irq/view"
 	"github.com/go-martini/martini"
-    "github.com/martini-contrib/render"
+	"github.com/martini-contrib/render"
 )
 
 func main() {
 	m := martini.Classic()
 
-    m.Use(render.Renderer(render.Options{
-        Layout: "base",
-    }))
+	m.Use(render.Renderer(render.Options{
+		Layout: "base",
+	}))
 
 	m.Get("/", view.Index)
 	m.Get("/latest", view.Latest)
@@ -22,6 +22,8 @@ func main() {
 	m.Get("/search", view.Search)
 	m.Get("/submit", view.Submit)
 	m.Get("/quote/:id", view.Quote)
+
+	m.Post("/submit", view.SubmitForm)
 
 	m.Run()
 }
--- view/submit.go
+++ view/submit.go
@@ -1,7 +1,11 @@
 package view
 
 import (
-    "github.com/martini-contrib/render"
+	"net/http"
+
+	"github.com/firba1/irq/model"
+
+	"github.com/martini-contrib/render"
 )
 
 func Submit(r render.Render) {
@@ -9,4 +13,34 @@ func Submit(r render.Render) {
 		"title": "Submit",
 	}
 	r.HTML(200, "submit", env)
+}
+
+func SubmitForm(r render.Render, request *http.Request) {
+	text := request.FormValue("text")
+	isOffensive := request.FormValue("is_offensive")
+	isNishbot := request.FormValue("is_nishbot")
+
+	db, err := model.NewModel("quotes.db")
+	if err != nil {
+		env := map[string]interface{}{
+			"title": "error",
+			"error": "what happen db conn no work",
+		}
+		r.HTML(500, "error", env)
+	}
+
+	err = db.AddQuote(model.Quote{
+		Text:        text,
+		IsOffensive: isOffensive == "on",
+		IsNishbot:   isNishbot == "on",
+	})
+	if err != nil {
+		env := map[string]interface{}{
+			"title": "error",
+			"error": "unable to add quote",
+		}
+		r.HTML(500, "error", env)
+	}
+
+	r.Redirect("/latest")
 }
