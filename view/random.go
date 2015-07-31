6edee6da31a8a5d55d5dace92214ed6161d8675f
Matthew Mulder <mmulder@yelp.com>
2015-07-30 19:47:23 -0700

    random

--- view/random.go
+++ view/random.go
@@ -1,5 +1,25 @@
 package view
 
-func Random() string {
-	return "Hello, world!"
+import (
+    "github.com/martini-contrib/render"
+    "github.com/firba1/irq/model"
+    "fmt"
+)
+
+func Random(r render.Render) {
+	db, err := model.NewModel("quotes.db")
+	if err != nil {
+		env := map[string]interface{}{
+			"title": "error",
+			"error": "db connection failed",
+		}
+		r.HTML(500, "error", env)
+		return
+	}
+
+	quotes, err := db.GetQuotes(model.Query{
+        Limit:   1,
+        OrderBy: []string{"random()"},
+    })
+    r.Redirect(fmt.Sprintf("/quote/%d", quotes[0].ID))
 }
