0f9b2341a0c2064523822c3db3ce00c1d023515a
Matthew Mulder <mmulder@yelp.com>
2015-08-20 17:09:38 -0700

    schema files for mysql

---
+++ model/schema/mysql/quote.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `quote` (
+  `id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `score` int(11) NOT NULL,
+  `is_offensive` tinyint(1) NOT NULL DEFAULT '0',
+  `is_nishbot` tinyint(1) NOT NULL DEFAULT '0',
+  `time_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
